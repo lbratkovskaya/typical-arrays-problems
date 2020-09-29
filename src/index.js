803974cfe0511f8518300a00dcc770c3718867a1
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-09-29 15:50:55 +0500

    feat: solve typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
+    if (!array || !Array.isArray (array) || array.length == 0)
   return 0;
+
+  let min = 0;
+  
+  array.forEach(element => {
+
+    min = Math.min(element, min);
+  });
+
+  return min
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray (array) || array.length == 0)
+    return 0;
+
+  let max = 0;
+
+  array.forEach(element => {
+    max = Math.max(element, max);
+  });
+
+  return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray (array) || array.length == 0)
+    return 0;
+
+  let sum = 0;
+
+  array.forEach(element => {
+    sum += element;
+  });
+
+  return sum / array.length;
 }
